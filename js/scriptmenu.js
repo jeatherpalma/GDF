99848984756c322070f04b0ec1d38d763c67963f
Jeather <loweroner33@gmail.com>
2017-05-01 23:34:08 -0500

    Se agrego el detalle producto a cada una de las categorias

--- js/scriptmenu.js
+++ js/scriptmenu.js
@@ -6,5 +6,35 @@ if(x.className==="menu"){
 else{x.className = "menu"}
 }
 
+/***Boton para cerrar imagen grande*/
+window.onload=function(){
+    
+    var bto=document.getElementById("cerrar");
+   
+    bto.onclick=cerrarImgGde;
+    function cerrarImgGde(){
+        document.getElementById("modal").style.display="none";
+    }
+    
+    var img=document.getElementsByClassName("imagenes");
+    for(var i=0; i<=img.length-1; i++){
+    img[i].onclick=mostrarImgGde;
+    }
+    function mostrarImgGde(){
+        var src1 = this.getAttribute("src");
+        document.getElementById("modal").style.display="block";
+        document.getElementById("img2").src=src1;
+        
+        
+        
+    }
+    
+    
+    
+
+    
+    
+}
+
 
 
